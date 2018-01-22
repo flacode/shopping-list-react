af0a05cee1f9bf88f84c738bddc5957ddd95ab9e
flacode <flavianshemerirwe@gmail.com>
2018-01-22 17:42:19 +0300

    [Feature #154506305] Add API call to login form

--- src/client.js
+++ src/client.js
@@ -1,27 +1,39 @@
 import axios from 'axios/lib/axios';
-
-const API = 'https://deployment-shopping-list-api.herokuapp.com/api/';
-// let token;
+// const BASE_URL = "https://deployment-shopping-list-api.herokuapp.com/api";
+const BASE_URL = 'http://127.0.0.1:5000/api';
+let token = null;
 let url;
+
+const handleError = (error, message) => {
+  // handle API generated errors
+  if (error.response) {
+    message(error.response.data.message);
+  } else {
+    // handle network server errors
+    message('Network error, please try again later');
+  }
+};
 
 // function to register a user on the API
 const registerUser = (user, success, message) => {
-  url = `${API}auth/register`;
+  url = `${BASE_URL}/auth/register`;
   axios.post(url, user)
-    .then((response) => {
-      success(response.data.message);
-    })
-    .catch((error) => {
-      // handle API generated errors
-      if (error.response) {
-        message(error.response.data.message);
-      } else {
-        // handle network server errors
-        message('Network error, please try again later');
-      }
-    });
+    .then(response => success(response.data.message))
+    .catch(error => handleError(error, message));
 };
 
-const Client = { registerUser };
+// API call to login user and get authentication token.
+const loginUser = (user, success, message) => {
+  url = `${BASE_URL}/auth/login`;
+  axios.post(url, user)
+    .then((response) => {
+      token = response.data.access_token;
+      localStorage.setItem('loggedIn', true);
+      return success(response.data.message);
+    })
+    .catch(error => handleError(error, message));
+};
+
+const Client = { registerUser, loginUser };
 
 export default Client;
--- src/components/account/loginForm.js
+++ src/components/account/loginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Container, Button, Form, FormGroup, Alert, Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 import Field from '../field';
 import Client from '../../client';
@@ -8,45 +8,62 @@ import logo from '../../imgs/shoppinglist.png';
 
 class LoginForm extends React.Component {
     state = {
-      users: [],
       fields: {
         username: '',
         password: '',
       },
       fieldErrors: {},
+      server: {
+        error: '',
+        message: '',
+        saveStatus: false,
+      },
     }
 
     onInputChange = ({ name, value, error }) => {
-      const { fields, fieldErrors } = this.state;
+      const fields = this.state.fields;
+      const fieldErrors = this.state.fieldErrors;
       fields[name] = value;
       fieldErrors[name] = error;
       this.setState({
         fields,
         fieldErrors,
-      });
-    }
-
-    onFormReset = (evt) => {
-      evt.preventDefault();
-      this.setState({
-        fields: {
-          username: '',
-          password: '',
+        server: {
+          error: '',
+          message: '',
         },
       });
     }
 
     onFormSubmit = (evt) => {
       evt.preventDefault();
-      const { user, users } = this.state;
+      const user = this.state.fields;
 
-      // validate before updating state
+      // validate fields before updating state
       if (this.validate()) return;
+
+      // send validated data to the server
+      this.setState({ server: { saveStatus: true } });
+      Client.loginUser(user, this.successServer, this.errorServer);
+      this.setState({ server: { saveStatus: false } });
+    }
+
+    // function to handle a successful API operation
+    successServer = (message) => {
       this.setState({
-        users: users.concat(user),
-        fields: {
-          username: '',
-          password: '',
+        server: {
+          error: false,
+          message,
+        },
+      });
+    }
+
+    // function to handle unsuccessful API operation
+    errorServer = (message) => {
+      this.setState({
+        server: {
+          error: true,
+          message,
         },
       });
     }
@@ -54,54 +71,61 @@ class LoginForm extends React.Component {
     validate = () => {
       const user = this.state.fields;
       const fieldErrors = this.state.fieldErrors;
+      const serverErrors = this.state.server;
       const errMessages = Object.keys(fieldErrors).filter(k => fieldErrors[k]);
       if (!user.username) return true;
       if (!user.password) return true;
       if (errMessages.length) return true;
+      if (serverErrors.error) return true;
       return false;
     }
 
     render() {
       return (
         <Container>
+          {
+            // check if there are no server errors then redirect to login
+            this.state.server.error === false ? <Redirect to="/shoppinglists" /> : null
+          }
           <Card className="card-container">
             <CardTitle className="thick-heading">SHOPPING LIST</CardTitle>
-              <CardImg className="img-card" src={logo} alt="Card image cap" />
-              <CardBody>
-                <h3 className="name-card"> Login</h3>
-                <Form className="form-signin">
-                  <FormGroup>
-                    <Field
-                      label="Username or Email"
-                      name="username"
-                      value={this.state.fields.username}
-                      onChange={this.onInputChange}
-                    />
+            <CardImg className="img-card" src={logo} alt="Card image cap" />
+            <CardBody>
+              <h3 className="name-card"> Login</h3>
+              {this.state.server.error && <Alert color="danger">{this.state.server.message}</Alert> }
+              <Form className="form-signin">
+                <FormGroup>
+                  <Field
+                    label="Username or Email"
+                    name="username"
+                    value={this.state.fields.username}
+                    onChange={this.onInputChange}
+                  />
                   <Link className="pull-right auth-reset" to="/reset-password">Forgot password?</Link>
-                    <Field
-                      label="Password:"
-                      name="password"
-                      type="password"
-                      value={this.state.fields.password}
-                      onChange={this.onInputChange}
-                    />
-                  </FormGroup>
-                  <div className="text-right">
-                    <Button
-                      className="btn-auth btn-signin"
-                      disabled={this.validate()}
-                      onClick={this.onFormSubmit}
-                      color="primary"
-                    >
+                  <Field
+                    label="Password:"
+                    name="password"
+                    type="password"
+                    value={this.state.fields.password}
+                    onChange={this.onInputChange}
+                  />
+                </FormGroup>
+                <div className="text-right">
+                  <Button
+                    className="btn-auth btn-signin"
+                    disabled={this.validate() && this.state.server.saveStatus}
+                    onClick={this.onFormSubmit}
+                    color="primary"
+                  >
                       Login
-                    </Button>
-                  </div>
-                </Form>
-                <br />
-                <p className="login">
+                  </Button>
+                </div>
+              </Form>
+              <br />
+              <p className="login">
                   Dont have an account, please sign up <Link to="/register" className="auth-link">here</Link>.
-                </p>
-              </CardBody>
+              </p>
+            </CardBody>
           </Card>
         </Container>
       );
